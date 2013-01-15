13f9938b8b85f28c2e01216b549eec764d88e075
Danny Peters <danmanstx@gmail.com>
2013-01-14 23:06:31 -0500

    finished chapter 10, added micropost

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -41,7 +41,7 @@ require 'spec_helper'
           end
       end   	
   	end
-    describe "authorization" do
+  describe "authorization" do
 
     describe "as non-admin user" do
       let(:user) { FactoryGirl.create(:user) }
@@ -88,6 +88,19 @@ require 'spec_helper'
         describe "visiting the user index" do
           before { visit users_path }
           it { should have_selector('title', text: 'Sign in') }
+        end
+      end
+
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { response.should redirect_to(signin_path) }
         end
       end
     end
